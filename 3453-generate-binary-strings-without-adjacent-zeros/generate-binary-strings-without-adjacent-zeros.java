18999f3540e5f0e26dd396b028de7d995bc43129
Prince kumar <126045290+princekumar987@users.noreply.github.com>
2025-01-26 17:11:03 +0530

    Time: 2 ms (87.18%) | Memory: 45.9 MB (47.47%) - LeetSync

--- 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.java
@@ -2,13 +2,6 @@ class Solution {
 
     public static void function(List<String>list,int n, StringBuilder sb,int lastchar){
         if(sb.length()==n){
-            boolean p=true;
-            for(int i=0;i<sb.length()-1;i++){
-                if(sb.charAt(i)=='0' && sb.charAt(i)==sb.charAt(i+1)){
-                    p=false;
-                }
-            }
-            if(p)
             list.add(sb.toString());
             return ;
         }
@@ -17,9 +10,11 @@ class Solution {
         function(list,n,sb,1);
         sb.deleteCharAt(sb.length()-1);
 
-        sb.append("0");
-        function(list,n,sb,0);
-        sb.deleteCharAt(sb.length()-1);
+        if(lastchar!=0){
+          sb.append("0");
+          function(list,n,sb,0);
+          sb.deleteCharAt(sb.length()-1);
+        }
 
     }
     public List<String> validStrings(int n) {
