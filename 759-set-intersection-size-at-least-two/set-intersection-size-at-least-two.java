1ad2f1e8faf91ce640ee0823ba2bc567b7470e09
Prince kumar <126045290+princekumar987@users.noreply.github.com>
2025-08-16 12:57:25 +0530

    Time: 76 ms (5.47%) | Memory: 45.5 MB (23.44%) - LeetSync

--- 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.java
+++ 759-set-intersection-size-at-least-two/set-intersection-size-at-least-two.java
@@ -3,42 +3,47 @@ class Solution {
         
 
         Arrays.sort(inter,Comparator.comparing(o->o[1]));
-       // System.out.println(Arrays.deepToString(inter));
+      
         ArrayList<Integer>list=new ArrayList<>();
+
         int a=inter[0][1];
         list.add(a);
         list.add(--a);
+        
         Collections.sort(list);
 
         for(int i=1;i<inter.length;i++){
+
                int c=inter[i][0];
                int d=list.get(list.size()-1);
+
                if(c==d){
+
                    list.add(inter[i][1]);
-                  // System.out.println(list+" "+i);
+
                }
                else if(c>d){
+
                   int e=inter[i][1];
                   list.add(e);
                   list.add(--e);
-                  //System.out.println(list+" "+i);
+
                }
                else if(c<d && c>list.get(list.size()-2)){
+
                      int g=inter[i][1];
                      if(d<inter[i][1]){
                         list.add(inter[i][1]);
                      }
                      else
                      list.add(--g);
-                     //System.out.println(list+" "+i);
+                     
                }
 
               Collections.sort(list);
               
         }
 
-        //System.out.println(list);
-        
 
         return list.size();
 
