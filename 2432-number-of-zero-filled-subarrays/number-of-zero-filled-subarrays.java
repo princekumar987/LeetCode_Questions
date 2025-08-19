36840293ea8fa979bff7079afaee20d48f8e1c83
Prince kumar <126045290+princekumar987@users.noreply.github.com>
2025-08-19 16:55:26 +0530

    Time: 3 ms (94.03%) | Memory: 61.9 MB (43.28%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.java
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.java
@@ -1,30 +1,21 @@
 class Solution {
     public long zeroFilledSubarray(int[] nums) {
-        if(nums.length==1 && nums[0]==0)return 1;
-        long count=1;
+        
         long ans=0;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]==0 && nums[i]==nums[i+1]){
+        long count=0;
+
+        for(int i=0;i<nums.length;i++){
+               if(nums[i]==0){
                 count++;
-                continue; 
-            }
-            else if(nums[i]==0 && count==1){
-                ans++;
-               continue;
-            }
-            else{
-                if(count==1){
-                    continue;
-                }
-                ans=ans+(count*(count+1))/2;
-                count=1;
-            }
+               }
+               else{
+                 ans=ans+(count*(count+1))/2;
+                 count=0;
+               }
         }
-        if(nums[nums.length-1]==0 && count==1){
-            ans++;
-        }
-        if(count>=2)
+
         ans=ans+(count*(count+1))/2;
+
         return ans;
     }
 }
