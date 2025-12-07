7baede1d8163bbc682eb8f17709af07403aed7b3
Prince kumar <126045290+princekumar987@users.noreply.github.com>
2025-12-07 22:46:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (10.29%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,32 +1,25 @@
 class Solution {
-    public boolean check(int[] nums) {
-        
-       int min=101;
-       int a=0;
-     
-
-       for(int i=0;i<nums.length-1;i++){
-          if(nums[i]>nums[i+1]){
+    public boolean check(int[] arr) {
+    
+     int a=0; 
+     for(int i=0;i<arr.length-1;i++){
+         if(arr[i]>arr[i+1]){
             a=i+1;
             break;
-          }
-       }
+         }
+     }
 
-       ArrayList<Integer>list=new ArrayList<>();
+     for(int i=a;i<arr.length-1;i++){
+          if(arr[i]>arr[i+1])return false;
+     }
 
-       for(int i=a;i<nums.length;i++){
-           list.add(nums[i]);
-       }
+     if(a!=0 && arr[arr.length-1]>arr[0])return false;
 
-       for(int i=0;i<a;i++){
-          list.add(nums[i]);
-       }
+     for(int i=0;i<a-1;i++){
+           if(arr[i]>arr[i+1])return false;
+     }
 
-       for(int i=0;i<list.size()-1;i++){
-           if(list.get(i)>list.get(i+1))return false;
-       }
-
-       return true;
+     return true;
 
     }
 }
