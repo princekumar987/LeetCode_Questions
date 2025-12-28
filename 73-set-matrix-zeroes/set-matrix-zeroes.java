32a67d977f384fdfbfb3395788edfddb3b9c6c03
Prince kumar <126045290+princekumar987@users.noreply.github.com>
2025-12-28 11:34:39 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (25.92%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -3,24 +3,62 @@ class Solution {
        
           int m=matrix.length;
           int n=matrix[0].length;
-          int []row=new int[m];
-          int []col=new int[n];
+          boolean p=false;
+          boolean q=false;
 
           for(int i=0;i<m;i++){
               for(int j=0;j<n;j++){
                   if(matrix[i][j]==0){
-                     row[i]=1;
-                     col[j]=1;
+                     if(i==0 &&  j==0){
+                        p=true;
+                        q=true;
+                        continue;
+                     }
+                     if(i==0){
+                        p=true;
+                        matrix[0][j]=0;
+                        continue;
+                     }
+                     if(j==0){
+                        q=true;
+                        matrix[i][0]=0;
+                        continue;
+                     }
+                      matrix[i][0]=0;
+                      matrix[0][j]=0;
                   }
               }
           }
 
-          for(int i=0;i<m;i++){
-             for(int j=0;j<n;j++){
-                 if(row[i]==1 || col[j]==1){
+          for(int i=1;i<m;i++){
+             for(int j=1;j<n;j++){
+                 if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j]=0;
                  }
              }
+          }
+
+          if(matrix[0][0]==0){
+               for(int i=0;i<m;i++){
+                  matrix[i][0]=0;
+               }
+               for(int j=0;j<n;j++){
+                  matrix[0][j]=0;
+               }
+          }
+          if(p){
+               for(int i=1;i<n;i++){
+                  matrix[0][i]=0;
+               }   
+          }
+
+          if(q){
+               for(int i=1;i<m;i++){
+                  matrix[i][0]=0;
+               }   
+          }
+          if(p || q){
+             matrix[0][0]=0;
           }
 
     }
