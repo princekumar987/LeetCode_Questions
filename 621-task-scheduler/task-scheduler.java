1b2417494192d2a82107f84abbfb7533dd176f88
Prince kumar <126045290+princekumar987@users.noreply.github.com>
2026-01-03 10:08:27 +0530

    Time: 122 ms (5.06%) | Memory: 48.8 MB (6.84%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -6,51 +6,54 @@ class Solution {
         for(int i=0;i<tasks.length;i++){
               mp.put(tasks[i],mp.getOrDefault(tasks[i],0)+1);
         }
-
-        PriorityQueue<Character>pq=new PriorityQueue<>((a,b)->mp.get(b)-mp.get(a));
-        pq.addAll(mp.keySet());
         int ans=tasks.length;
-        List<Character>list=new ArrayList<>();
+
+        PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
+        for(Character c:mp.keySet()){
+            pq.add(mp.get(c));
+        }
+        
+        List<Integer>list=new ArrayList<>();
 
         while(pq.size()>0){
-              
-              if(pq.size()-1>=n){
-                  int a=n+1;
-                  while(a-->0){
-                    char ch=pq.remove();
-                    if(mp.get(ch)==1)mp.remove(ch);
-                    else{
-                        mp.put(ch,mp.get(ch)-1);
-                        list.add(ch);
-                    }
-                  }
-                  for(int i=0;i<list.size();i++){
-                      pq.add(list.get(i));
-                  }
-                  list.clear();
-              }
-              else{
-                   int a=n-pq.size()+1;
-                   int x=pq.size();
-                   while(x-->0){
-                       char ch=pq.remove();
-                       if(mp.get(ch)==1)mp.remove(ch);
-                       else{
-                          mp.put(ch,mp.get(ch)-1);
-                          list.add(ch);
+               
+               if(pq.size()-1>=n){
+                     int a=n+1;
+                     while(a-->0){
+                        int df=pq.remove();
+                        if(df==1);
+                        else{
+                            list.add(df-1);
                         }
+                     }
+
+                     for(int i=0;i<list.size();i++){
+                        pq.add(list.get(i));
+                     }
+                     list.clear();
+               }
+               else{
+                   int a=n-pq.size()+1;
+                   int fg=pq.size();
+
+                   while(fg-->0){
+                       int fh=pq.remove();
+                       if(fh==1)continue;
+                       else{
+                          list.add(fh-1);
+                       }
                    }
 
                    for(int i=0;i<list.size();i++){
-                      pq.add(list.get(i));
-                   } 
-                   if(pq.size()>0)ans=ans+a;
+                       pq.add(list.get(i));
+                   }
                    list.clear();
-              }
 
-              
+                   if(pq.size()>0)ans=ans+a;
+               }
+               System.out.println(pq);
         }
-
+        
 
         return ans;
     }
