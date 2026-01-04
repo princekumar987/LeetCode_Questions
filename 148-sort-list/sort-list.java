71d4fb92ed0d0fc4f745b43d41a5d8c4ea67887a
Prince kumar <126045290+princekumar987@users.noreply.github.com>
2026-01-04 16:06:03 +0530

    Time: 11 ms (36.94%) | Memory: 59.3 MB (56.11%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -9,26 +9,69 @@
  * }
  */
 class Solution {
+
+    static ListNode findMiddle(ListNode head){
+         
+         ListNode slow=head;
+         ListNode fast=head.next;
+
+         while(fast!=null && fast.next!=null){
+              slow=slow.next;
+              fast=fast.next.next;
+         }
+
+         return slow;
+    }
+
+    static ListNode mergeSortedList(ListNode list1,ListNode list2){
+
+         ListNode head=new ListNode(-1);
+         ListNode temp1=list1;
+         ListNode temp2=list2;
+         ListNode temp=head;
+
+         while(temp1!=null && temp2!=null){
+               
+               if(temp1.val>=temp2.val){
+                    temp.next=temp2;
+                    temp=temp.next;
+                    temp2=temp2.next;
+               }
+               else{
+                   temp.next=temp1;
+                   temp=temp.next;
+                   temp1=temp1.next;
+               }
+         }  
+
+         while(temp1!=null){
+            temp.next=temp1;
+            temp=temp.next;
+            temp1=temp1.next;
+         }
+
+         while(temp2!=null){
+            temp.next=temp2;
+            temp=temp.next;
+            temp2=temp2.next;
+         }
+
+        return  head.next;
+
+    }
+
     public ListNode sortList(ListNode head) {
-        if(head==null)    return head;
 
-        ArrayList<Integer>list=new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null){
-            list.add(temp.val);
-            temp=temp.next;
-        }
-        Collections.sort(list);
+        if(head==null || head.next==null) return head;
 
-        temp=head;
-        int i=0;
-        while(temp!=null){
-            temp.val=list.get(i);
-            temp=temp.next;
-            i++;
-        }
-        
-        return head;
-        
+        ListNode middle=findMiddle(head);
+        ListNode right=middle.next;
+        middle.next=null;
+        ListNode left=head;
+
+        left=sortList(left);
+        right=sortList(right);
+
+        return mergeSortedList(left,right);   
     }
 }
