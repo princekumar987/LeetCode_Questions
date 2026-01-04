5f4c960a20a5aa64cec00b5f8ff6f694edfdd14d
Prince kumar <126045290+princekumar987@users.noreply.github.com>
2026-01-04 23:10:32 +0530

    Time: 3 ms (41.84%) | Memory: 47.9 MB (61.26%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -5,7 +5,7 @@ class Solution {
 
          for(int i=0;i<arr.length-1;i++){
               max=Math.max(max,arr[i]+i);
-              if(arr[i]==0 && max<=i)return false;
+              if(max<=i)return false;
          }
 
          return true;
