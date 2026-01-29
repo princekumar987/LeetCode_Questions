3eb6d11d6d7fa91b0cd71078cc252f4259725bc3
Prince kumar <126045290+princekumar987@users.noreply.github.com>
2026-01-29 23:00:16 +0530

    Time: 15 ms (51.37%) | Memory: 47.9 MB (18.99%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -7,15 +7,20 @@ class Solution {
           mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
        }
 
-       PriorityQueue<Integer>pq=new PriorityQueue<>((a,b)->mp.get(b)-mp.get(a));
-       pq.addAll(mp.keySet());
-
-       int []ans=new int[k];
-       int j=0;
-       while(k-->0){
-           ans[j++]=pq.remove();
+       PriorityQueue<Integer>pq=new PriorityQueue<>((a,b)->mp.get(a)-mp.get(b));
+       
+       for(Integer e:mp.keySet()){
+             pq.add(e);
+             if(pq.size()>k)pq.remove(); 
        }
 
+       int []ans=new int[k];
+       
+       int j=0;
+       while(pq.size()>0){
+          ans[j++]=pq.remove();
+       } 
+       
        return ans;
 
     }
