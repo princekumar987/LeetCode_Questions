6f7b36a2ee1a5525374f8e77abe5a28b7bb8cdb8
Prince kumar <126045290+princekumar987@users.noreply.github.com>
2026-01-29 23:22:37 +0530

    Time: 1 ms (99.66%) | Memory: 46.8 MB (57.04%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -47,19 +47,23 @@ class Solution {
 
     }
 
+    static ListNode merge(ListNode[] lists,int  start, int end){
+           
+           if(start==end)return lists[start];
+
+           int mid=start+(end-start)/2;
+
+           ListNode l1=merge(lists,start,mid);
+           ListNode l2=merge(lists,mid+1,end);
+
+           return merge(l1,l2);
+    }
+
     public ListNode mergeKLists(ListNode[] lists) {
-       
-       if(lists.length==0)return null;
+          
+          if(lists==null || lists.length==0)return null;
 
-       ListNode head=lists[0];
-       ListNode curr=head;
-       
-       for(int i=1;i<lists.length;i++){
-           head= merge(head,lists[i]);
-       }
-
-       return head;
-
+          return merge(lists,0,lists.length-1);
 
     }
 }
